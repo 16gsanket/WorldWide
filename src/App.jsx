deafcaa5874a25961e09873a8a21682615e82f1d
16gsanket <16gsanket@gmail.com>
2024-03-09 17:54:01 +0530

    COntext custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from "react";
-
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -11,60 +9,33 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
+import { CityProvider, City_C } from "./contexts/CitiesContext";
 
 const BASE_URL = "http://localhost:8000";
 
 function App() {
-  /*const [cities, setCities] = useState([]);
-  const [IsLoading, setIsLoading] = useState(false);
-
-  useEffect(function () {
-    async function fetchcities() {
-      try {
-        setIsLoading(true);
-        let res = await fetch(`${BASE_URL}/cities`);
-        let data = await res.json();
-
-        setCities((citydata) => (citydata = data));
-        console.log(data);
-      } catch (err) {
-        console.log("error in fetching data");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    fetchcities();
-  }, []);
-
-  */
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Homepage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="login" element={<Login />} />
-        <Route path="pagelayout" element={<Pagelayout />}>
-          <Route index element={<Navigate replace to="cities" />} />
-          <Route
-            path="cities"
-            element={<CityList cities={cities} IsLoading={IsLoading} />}
-          />
+    <CityProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
+          <Route path="pagelayout" element={<Pagelayout />}>
+            <Route index element={<Navigate replace to="cities" />} />
+            <Route path="cities" element={<CityList />} />
 
-          <Route path="cities/:id" element={<City />} />
-          <Route
-            path="country"
-            element={<CountryList cities={cities} IsLoading={IsLoading} />}
-          />
-          <Route path="form" element={<Form />} />
-        </Route>
-        <Route path="*" element={<Pagenotfound />} />
+            <Route path="cities/:id" element={<City />} />
+            <Route path="country" element={<CountryList />} />
+            <Route path="form" element={<Form />} />
+          </Route>
+          <Route path="*" element={<Pagenotfound />} />
 
-        <Route />
-      </Routes>
-    </BrowserRouter>
+          <Route />
+        </Routes>
+      </BrowserRouter>
+    </CityProvider>
   );
 }
 
--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,21 +2,23 @@ import styles from "./CountryList.module.css";
 import Spinner from "./Spinner";
 import CountryItem from "./CountryItem";
 import Message from "./Message";
+import { City_C } from "../contexts/CitiesContext";
 
-function CountryList({ cities, IsLoading }) {
+function CountryList() {
+  const { cities, IsLoading } = City_C();
+
   if (IsLoading) return <Spinner />;
   if (!cities.length) return <Message />;
 
-  const countries = cities.reduce((arr,city)=>{
-    if(!arr.map((el)=>el.country).includes(city.country))
-        return [...arr , {country:city.country , emoji:city.emoji}]
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
     else return arr;
-  },[])
+  }, []);
 
-  console.log(countries)
+  console.log(countries);
   return (
     <>
-      
       <ul className={styles.countryList}>
         {countries.map((country) => (
           <CountryItem country={country} />
